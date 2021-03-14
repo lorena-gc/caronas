000880c42a94af8422a37246ef66c8f3a74342a8
Lorena Gomes <logcy@yahoo.com.br>
2021-03-13 22:09:40 -0300

    Configuração inicial

---
+++ src/main/java/com/logcy/caronas/CaronasApplication.java
@@ -0,0 +1,13 @@
+package com.logcy.caronas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaronasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logcy/caronas/CaronasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logcy.caronas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
